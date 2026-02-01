5b928fb7940f4566cc15982514b8866045f031b0
Danil Platonov <platonov.danil.ivanovich@gmail.com>
2026-02-01 01:13:06 -0800

    cleanup

--- src/board.gd
+++ src/board.gd
@@ -14,6 +14,29 @@ func xy2pos(x_i, y_i):
 	var z = x0*0.0 - 0.0009*y0 + 1
 	return Vector2(360+1.08*x/z, 100+0.85*y/z - 10)
 
+func new_piece(file, rank):
+	var y_i = rank - 1
+	var x_i = file - 1
+	var p1 = piece.instantiate()
+
+	p1.position = xy2pos(x_i, y_i)
+	p1.modulate = Color(x_i/8.0, y_i/8.0, 1)
+	#p1.scale = Vector2(4,4)
+	# only for classic chess pieces provided
+	p1.get_node("Sprite2D").scale = Vector2(0.15,0.15)
+	var area2d = p1.get_node("Area2D")
+	area2d.x_i = x_i
+	area2d.y_i = y_i
+	area2d.piece_selected.connect(_on_piece_selected)
+	area2d.piece_grabbed.connect(_on_piece_grabbed)
+	area2d.piece_dropped.connect(_on_piece_dropped)
+
+	area2d.file = file
+	area2d.rank = rank
+	
+	add_child(p1)
+	return area2d
+
 # TODO: piece is actually a square that might have a piece (this allows selecting empty squares to move to)
 @export var piece: PackedScene
 # Called when the node enters the scene tree for the first time.
@@ -22,35 +45,18 @@ func _ready() -> void:
 	#	for x_i in range(8):
 	for rank in range(8, 0, -1):
 		for file in range(1, 9):
-			var y_i = rank - 1
-			var x_i = file - 1
-			var p1 = piece.instantiate()
-
-			p1.position = xy2pos(x_i, y_i)
-			p1.modulate = Color(x_i/8.0, y_i/8.0, 1)
-			#p1.scale = Vector2(4,4)
-			# only for classic chess pieces provided
-			p1.get_node("Sprite2D").scale = Vector2(0.15,0.15)
-			var area2d = p1.get_node("Area2D")
-			area2d.x_i = x_i
-			area2d.y_i = y_i
-			area2d.piece_selected.connect(_on_piece_selected)
-			area2d.piece_grabbed.connect(_on_piece_grabbed)
-			area2d.piece_dropped.connect(_on_piece_dropped)
-			
-			# for rank in range(8, 0, -1):
-			#   for file in range(1, 9):
-			#
-			#var file = x_i  # ???
-			#var rank = y_i # ???
-			#area2d.index = y_i * 8 + x_i # ???
-			area2d.file = file
-			area2d.rank = rank
+			var area2d = new_piece(file, rank)
 			area2d.index = Chess.square_index(file, rank)
 			print(area2d.index)
 			area2d.san_name = Chess.square_get_name(area2d.index)
-			
-			add_child(p1)
+
+# NO YOU DONT! We have a board already filled with pieces, so you just gotta
+# highlight already placed pieces instead of creating new ones
+#func add_dummy_piece(idx):
+	#print("adding dummy piece at ", idx)
+	#var file = idx / 8;
+	#var rank = idx % 8; # probably
+	#var area2d = new_piece(file, rank)
 
 func _on_piece_selected(x_i, y_i):
 	print(x_i, y_i)
@@ -71,16 +77,14 @@ func _on_piece_dropped(from, to):
 func _process(delta: float) -> void:
 	pass
 
+func mark_possible_moves(ids):
+	pass
 
 func setup_board(chess: Chess) -> void:
-	#print(chess.pieces)
 	for child in get_children():
-		#print(child.scene_file_path == 'res://piece.tscn')
 		if child.scene_file_path != 'res://piece.tscn': continue
-		var piece_node = child
-		if not piece_node: continue
-		var square = piece_node.get_node("Area2D")
-		var sprite = piece_node.get_node("Sprite2D")
+		var square = child.get_node("Area2D")
+		var sprite = child.get_node("Sprite2D")
 		var piece = chess.pieces[square.index]
 		if piece != null:
 			var col := "b" if Chess.piece_color(piece) else "w"

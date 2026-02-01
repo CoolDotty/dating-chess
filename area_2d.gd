195311c6e2463a74e363d145e81940ce3e06399e
Danil Platonov <platonov.danil.ivanovich@gmail.com>
2026-01-31 21:09:00 -0800

    WIP cool chess

---
+++ area_2d.gd
@@ -0,0 +1,42 @@
+extends Area2D
+
+signal piece_selected(x_i, y_i);
+
+var x_i;
+var y_i;
+
+var selected;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	selected = false
+	#pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_mouse_entered():
+	print("entered")
+	get_parent().modulate = Color.WEB_GRAY  # Highlight sprite parent
+
+var selected_color = Color.PALE_GREEN
+
+func _on_mouse_exited():
+	if not selected:
+		get_parent().modulate = Color.WHITE
+	else:
+		get_parent().modulate = selected_color
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		selected = true
+		piece_selected.emit(x_i, y_i)
+		get_parent().modulate = selected_color
+		print(viewport, event, shape_idx)
+		
+func unselect():
+	selected = false
+	get_parent().modulate = Color.WHITE

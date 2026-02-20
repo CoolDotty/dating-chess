e7f53d18eb401664f551d064507075beab4143ca
Dot Phillips <dot@x3c.ca>
2026-02-20 14:18:51 -0800

    Code up intro (#20)
    * add intro
    
    * oops fix queen bug

--- src/Intro/intro_tutorial.gd
+++ src/Intro/intro_tutorial.gd
@@ -1,9 +1,16 @@
 extends Node2D
 
+const GAME = preload("uid://cw1fs4vp0p4tm")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Dialogic.start("IntroTimeline") # Replace with function body.
+	Dialogic.timeline_ended.connect(_timeline_ended)
+
+
+func _timeline_ended():
+	await $"Fade out".fade_to_black()
+	get_tree().change_scene_to_packed(GAME)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
